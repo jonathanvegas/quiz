0fe064aa6895f5337daad6677f5703af08070205
Jonathan Vegas <jonathanvegas@Jonathans-MacBook-Air.local>
2022-07-15 11:21:25 -0400

    Weekly quizzes

---
+++ quiz1w1.js
@@ -0,0 +1,22 @@
+// const student = {
+//     name: 'Jonathan',
+//     age: 42,
+//     lost: false,
+// }
+// student.name = "Antonio"
+// console.log(student)
+
+// let num1 = 10
+// let num2 = 5
+// function addNums(num1, num2){
+//     return num1 + num2
+// }
+// console.log('Result: ' + addNums(num1,num2))
+
+
+function countDown () {  
+    for (i = 10; i <= 1; i--){
+        console.log(i)
+    }
+}
+countDown()
